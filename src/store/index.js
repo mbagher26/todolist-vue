e24f8601a2cdda0088d16801f4e26ce5cf91ddf8
unknown <mohammadbaghermollanory@gmail.com>
2024-12-13 19:47:15 +0330

    SET_POSTS mutations & fetchPosts actions & export default store

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,23 @@
 import { createStore } from 'vuex';
+import api from '../api/api';
 
 // Create a new store instance.
 const store = createStore({
   state: {
-   
+   posts : []
   },
 
   mutations: {
-   
+   SET_POSTS(state , posts){
+    state.posts = posts
+   }
   },
 
   actions: {
-
+    async fetchPosts({commit}){
+        const response = await api.getItems();
+        commit('SET_POSTS', response.data)
+    }
   },
 
   getters: {
@@ -19,4 +25,5 @@ const store = createStore({
   }
 
 
-})
+});
+export default store
