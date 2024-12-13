fe57681c11e190f7bb9da831c17f8d4e6807d18e
unknown <mohammadbaghermollanory@gmail.com>
2024-12-13 19:45:15 +0330

    get posts from state managemet and display in template whith style css

---
+++ src/pages/posts/Posts.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+    <h1>Posts List</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.id" class="post-item">
+        <h2>{{ post.title }}</h2>
+        <p><strong>User ID:</strong> {{ post.userId }}</p>
+        <p>{{ post.body }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import { useStore } from 'vuex';
+
+
+const store = useStore();
+const posts = computed(() => store.state.posts)
+store.dispatch("fetchPosts")
+
+</script>
+
+<style>
+.post-item {
+  border: 1px solid #ddd;
+  padding: 10px;
+  margin-bottom: 10px;
+  border-radius: 5px;
+  background-color: #f9f9f9;
+}
+
+.post-item h2 {
+  margin: 0 0 5px;
+  font-size: 1.2rem;
+  color: #333;
+}
+
+.post-item p {
+  margin: 5px 0;
+  line-height: 1.5;
+  color: #555;
+}
+</style>
