69eed06ba9ef9af5614980627ff70b1dd8b919bb
unknown <mohammadbaghermollanory@gmail.com>
2024-12-14 09:08:45 +0330

    create post state & SET_POST mutation & fetchPostById action

--- src/store/index.js
+++ src/store/index.js
@@ -4,20 +4,34 @@ import api from '../api/api';
 // Create a new store instance.
 const store = createStore({
   state: {
-   posts : []
+   posts : [],
+   post: {}
   },
 
   mutations: {
    SET_POSTS(state , posts){
     state.posts = posts
-   }
+   },
+
+   SET_POST(state , post){
+    state.post = post
+   },
   },
 
   actions: {
     async fetchPosts({commit}){
         const response = await api.getItems();
         commit('SET_POSTS', response.data)
-    }
+    },
+
+    async fetchPostById({commit}, id){
+      try{
+        const response = await api.getItem(id);
+        commit('SET_POST', response.data)
+      }catch (erroe){
+        console.error('Error for fetch by Id', erroe);
+      }
+    },
   },
 
   getters: {
