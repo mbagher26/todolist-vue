0bfadd75c19aa37e5e47b9204fce6716779422b6
unknown <mohammadbaghermollanory@gmail.com>
2024-12-14 10:26:42 +0330

    define REMOVE_POST mutation and delete action

--- src/store/index.js
+++ src/store/index.js
@@ -4,34 +4,48 @@ import api from '../api/api';
 // Create a new store instance.
 const store = createStore({
   state: {
-   posts : [],
-   post: {}
+    posts: [],
+    post: {}
   },
 
   mutations: {
-   SET_POSTS(state , posts){
-    state.posts = posts
-   },
+    SET_POSTS(state, posts) {
+      state.posts = posts
+    },
 
-   SET_POST(state , post){
-    state.post = post
-   },
+    SET_POST(state, post) {
+      state.post = post
+    },
+
+    REMOVE_POST(state, id) {
+      state.posts = state.posts.filter(post => post.id !== id)
+    }
   },
 
   actions: {
-    async fetchPosts({commit}){
-        const response = await api.getItems();
-        commit('SET_POSTS', response.data)
+    async fetchPosts({ commit }) {
+      const response = await api.getItems();
+      commit('SET_POSTS', response.data)
     },
 
-    async fetchPostById({commit}, id){
-      try{
+    async fetchPostById({ commit }, id) {
+      try {
         const response = await api.getItem(id);
-        commit('SET_POST', response.data)
-      }catch (erroe){
+        commit('SET_POST', response.data);
+      } catch (erroe) {
         console.error('Error for fetch by Id', erroe);
       }
     },
+
+    async delete({ commit }, id) {
+      try {
+        const response = await api.deleteItem(id);
+        commit("REMOVE_POST", id)
+
+      } catch (erroe) {
+        console.error('Delete failed', erroe);
+      }
+    }
   },
 
   getters: {
